b561256ce75f2e44df25b0729d2310ad6f4438af
digitaldias <pedro@digitaldias.com>
2024-08-04 10:49:48 +0200

    Add an Id to Boats

--- src/TestApp/Boats/Boat.cs
+++ src/TestApp/Boats/Boat.cs
@@ -2,8 +2,11 @@
 
 namespace TestApp.Boats;
 
-public sealed class Boat(string boatName) : IAmEntity
+public sealed class Boat(string boatName, Guid? boatId = null) : IAmEntity
 {
+    public Guid Id { get; init; } = boatId ?? Guid.NewGuid();
+
     public string Name { get; init; } = boatName;
+
     public DateTime Created { get; init; } = DateTime.UtcNow;
 }
