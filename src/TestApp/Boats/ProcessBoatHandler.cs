3d33288d8533cecac85d74eb56eb371e1411594e
digitaldias <pedro@digitaldias.com>
2024-08-04 10:50:34 +0200

    Simulate request in ProcessBoatHandler using requestcatcher.com

--- src/TestApp/Boats/ProcessBoatHandler.cs
+++ src/TestApp/Boats/ProcessBoatHandler.cs
@@ -1,21 +1,39 @@
-﻿using Mediator;
+﻿using System.Text;
+using System.Text.Json;
+using Mediator;
 using Microsoft.Extensions.Logging;
 using TestApp.Models;
 
 namespace TestApp.Boats;
 
-public sealed partial class ProcessBoatHandler(ILogger<ProcessBoatHandler> logger) : IRequestHandler<ProcessBoat, Result<bool>>
+public sealed partial class ProcessBoatHandler(ILogger<ProcessBoatHandler> logger, IHttpClientFactory clientFactory) : IRequestHandler<ProcessBoat, Result<bool>>
 {
-    public ValueTask<Result<bool>> Handle(ProcessBoat request, CancellationToken cancellationToken)
+    private readonly HttpClient _httpClient = clientFactory.CreateClient();
+
+    public async ValueTask<Result<bool>> Handle(ProcessBoat request, CancellationToken cancellationToken)
     {
-        if (request.TheBoat.Name.StartsWith('W'))
+        if (request.TheBoat.Name.StartsWith('F'))
         {
             LogBoatOnDryLand(logger, request.MessageType, request.TheBoat.Name);
             var result = new Result<bool>(new Problem("This boat is on dry land waiting to be scrapped"));
-            return ValueTask.FromResult(result);
+            return result;
         }
 
-        return ValueTask.FromResult(new Result<bool>(true));
+        var boatJson = JsonSerializer.Serialize(request.TheBoat);
+        var httpRequest = new HttpRequestMessage(HttpMethod.Post, "https://testnavigatetohandler.requestcatcher.com/test");
+        httpRequest.Headers.Add("Command", request.MessageType);
+        httpRequest.Headers.Add("BoatId", request.TheBoat.Id.ToString());
+        httpRequest.Content = new StringContent(boatJson, Encoding.UTF8, "application/json");
+        var response = await _httpClient.SendAsync(httpRequest, cancellationToken);
+        if (response.IsSuccessStatusCode)
+        {
+            return new Result<bool>(true);
+        }
+        else
+        {
+            var problem = new Problem("Failed to send the boat");
+            return new Result<bool>(problem);
+        }
     }
 
     [LoggerMessage(1, LogLevel.Warning, "{Command}: Boat {BoatName} is on dry land waiting to be scrapped", SkipEnabledCheck = true)]
